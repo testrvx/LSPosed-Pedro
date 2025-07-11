2bca8dee027af7d8d1f5ecc790a9c6c481c4ab37
ThePedroo <pedroolimpioguerra@gmail.com>
2025-07-11 12:36:22 -0300

    ...

--- dex2oat/src/main/cpp/oat_hook.cpp
+++ dex2oat/src/main/cpp/oat_hook.cpp
@@ -8,76 +8,28 @@
 
 #include "logging.h"
 
-const std::string_view parameter_to_remove = " --inline-max-code-units=0";
+// const std::string_view parameter_to_remove = " --inline-max-code-units=0";
 
 #define DCL_HOOK_FUNC(ret, func, ...)                                                              \
     ret (*old_##func)(__VA_ARGS__);                                                                \
     ret new_##func(__VA_ARGS__)
 
-bool store_updated = false;
+DCL_HOOK_FUNC(size_t, _ZN3art15CompilerOptionsC1Ev, void *header) {
+    size_t size = _ZN3art15CompilerOptionsC1Ev(header);
 
-void UpdateKeyValueStore(std::map<std::string, std::string>* key_value, uint8_t* store) {
-    LOGD("updating KeyValueStore");
-    char* data_ptr = reinterpret_cast<char*>(store);
-    if (key_value != nullptr) {
-        auto it = key_value->begin();
-        auto end = key_value->end();
-        for (; it != end; ++it) {
-            strlcpy(data_ptr, it->first.c_str(), it->first.length() + 1);
-            data_ptr += it->first.length() + 1;
-            strlcpy(data_ptr, it->second.c_str(), it->second.length() + 1);
-            data_ptr += it->second.length() + 1;
+    for (int i = 0LL; i < 80 ; i += sizeof(void *)) {
+        if (header + i == -1LL) {
+            LOGI("Found a null pointer in CompilerOptions at offset %d", i);
+
+            (header + i) = 0LL;
+
+            return size;
         }
     }
-    LOGD("KeyValueStore updated");
-    store_updated = true;
-}
 
-DCL_HOOK_FUNC(uint32_t, _ZNK3art9OatHeader20GetKeyValueStoreSizeEv, void* header) {
-    uint32_t size = old__ZNK3art9OatHeader20GetKeyValueStoreSizeEv(header);
-    if (store_updated) {
-        LOGD("OatHeader::GetKeyValueStoreSize() called on object at %p\n", header);
-        size = size - parameter_to_remove.size();
-    }
+    LOGE("CompilerOptions header is not null at any offset, this is unexpected");
+
     return size;
-}
-
-DCL_HOOK_FUNC(uint8_t*, _ZNK3art9OatHeader16GetKeyValueStoreEv, void* header) {
-    LOGD("OatHeader::GetKeyValueStore() called on object at %p\n", header);
-    uint8_t* key_value_store_ = old__ZNK3art9OatHeader16GetKeyValueStoreEv(header);
-    uint32_t key_value_store_size_ = old__ZNK3art9OatHeader20GetKeyValueStoreSizeEv(header);
-    const char* ptr = reinterpret_cast<const char*>(key_value_store_);
-    const char* end = ptr + key_value_store_size_;
-    std::map<std::string, std::string> new_store = {};
-
-    LOGD("scanning [%p-%p] for oat headers", ptr, end);
-    while (ptr < end) {
-        // Scan for a closing zero.
-        const char* str_end = reinterpret_cast<const char*>(memchr(ptr, 0, end - ptr));
-        if (str_end == nullptr) [[unlikely]] {
-            LOGE("failed to find str_end");
-            return key_value_store_;
-        }
-        std::string_view key = std::string_view(ptr, str_end - ptr);
-        const char* value_start = str_end + 1;
-        const char* value_end =
-            reinterpret_cast<const char*>(memchr(value_start, 0, end - value_start));
-        if (value_end == nullptr) [[unlikely]] {
-            LOGE("failed to find value_end");
-            return key_value_store_;
-        }
-        std::string_view value = std::string_view(value_start, value_end - value_start);
-        LOGV("header %s:%s", key.data(), value.data());
-        if (key == "dex2oat-cmdline") {
-            value = value.substr(0, value.size() - parameter_to_remove.size());
-        }
-        new_store.insert(std::make_pair(std::string(key), std::string(value)));
-        // Different from key. Advance over the value.
-        ptr = value_end + 1;
-    }
-    UpdateKeyValueStore(&new_store, key_value_store_);
-
-    return key_value_store_;
 }
 
 #undef DCL_HOOK_FUNC
@@ -107,8 +59,7 @@ __attribute__((constructor)) static void initialize() {
         }
     }
 
-    PLT_HOOK_REGISTER(dev, inode, _ZNK3art9OatHeader20GetKeyValueStoreSizeEv);
-    PLT_HOOK_REGISTER(dev, inode, _ZNK3art9OatHeader16GetKeyValueStoreEv);
+    PLT_HOOK_REGISTER(dev, inode, _ZN3art15CompilerOptionsC1Ev);
     if (lsplt::CommitHook()) {
         LOGD("lsplt hooks done");
     };
